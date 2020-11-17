1df54be6b6dab5d26e49e4baa1135e1a85e27ca6
Justin R. Zweifel <jzweifel15@yahoo.com>
2020-11-17 03:04:32 +0000

    Automatically backed up by Learn

--- app/services/sighting_serializer.rb
+++ app/services/sighting_serializer.rb
@@ -5,7 +5,18 @@ class SightingSerializer
   end
   
   def to_serialized_json
-    
+    options = {
+      include: {
+        bird: {
+          only: [:name, :species]
+        },
+        location: {
+          only: [:latitude, :longitude]
+        }
+      },
+      except: [:updated_at],
+    }
+    @sighting.to_json(options)
   end
   
 end
